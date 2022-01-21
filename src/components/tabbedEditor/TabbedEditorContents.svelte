7b6c097f8b0d62c77d478645fdddf1f5fe19f84c
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-01-21 10:20:42 +0530

    Merge EditorTabContent and TabbedEditorContents

--- src/components/tabbedEditor/TabbedEditorContents.svelte
+++ src/components/tabbedEditor/TabbedEditorContents.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
     import Tabs from "../tabs/Tabs.svelte"
-    import EditorTabContent from "./EditorTabContent.svelte"
+    import Editor from "../Editor.svelte"
+    import TabContent from "../tabs/TabContent.svelte"
     import type TabsContext from "../tabs/tabsContext"
 
     export let fontSize: number = 17
     export let ctx: TabsContext
 
     const tabsList = ctx.tabsList
+    const editors: { [key: string]: Editor } = {}
 </script>
 
 <Tabs {ctx}>
     {#each $tabsList as tabName}
-        <EditorTabContent {tabName} {fontSize} />
+        <TabContent {tabName} on:appear={() => editors[tabName].focus()}>
+            <Editor {fontSize} editorId={tabName} bind:this={editors[tabName]} />
+        </TabContent>
     {/each}
 </Tabs>
