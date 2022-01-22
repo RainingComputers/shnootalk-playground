0bf5d362d83c573d1dcdac720f8dde04b448838b
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-01-23 01:41:01 +0530

    Minor cleanup CSS

--- src/appComponents/PlaygroundLogo.svelte
+++ src/appComponents/PlaygroundLogo.svelte
@@ -19,7 +19,7 @@
 <div class={labelClass} on:click={onClick}>
     <img src="icons/logo.svg" class="" width="50px" alt="" />
     <span class="box">
-        <span class="pad-left-small text-no-wrap font-small">
+        <span class="pad-left-small text-no-wrap">
             ShnooTalk Playground
         </span>
     </span>
