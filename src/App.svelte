2cfd1c98bc4d1ac80290a599be714bc5fa0a8983
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-01-28 20:10:24 +0530

    Fix App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { CompileResult, dispatchProgram } from "./api/cloudCompile"
     import Toolbar from "./components/Toolbar.svelte"
     import RunButton from "./components/RunButton.svelte"
     import PlaygroundLogo from "./appComponents/PlaygroundLogo.svelte"
@@ -10,7 +11,6 @@
     import TextInput from "./components/TextInput.svelte"
     import About from "./appComponents/About.svelte"
     import OutputPanel from "./appComponents/OutputPanel.svelte"
-    import { CompileResult, dispatchProgram } from "./api/cloudCompile"
     import InputPanel from "./appComponents/InputPanel.svelte"
 
     const tabbedEditorContext = new TabsContext(["main.shtk"])
@@ -65,52 +65,57 @@
 </script>
 
 <svelte:window on:keydown={onKeyDown} />
-<svelte:body class="box-root" />
 
-<Modal
-    width={25}
-    bind:this={newTabModal}
-    onClose={() => {
-        tabbedEditrorContents.focus()
-    }}
->
-    <TextInput
-        label="Enter name"
-        bind:this={newTabTextInput}
-        onEnterCallback={closeNewTabModalAndCreateTab}
-    />
-</Modal>
+<main class="box-root">
+    <Modal
+        width={25}
+        bind:this={newTabModal}
+        onClose={() => {
+            tabbedEditrorContents.focus()
+        }}
+    >
+        <TextInput
+            label="Enter name"
+            bind:this={newTabTextInput}
+            onEnterCallback={closeNewTabModalAndCreateTab}
+        />
+    </Modal>
 
-<Modal
-    width={30}
-    bind:this={aboutModal}
-    onClose={() => {
-        tabbedEditrorContents.focus()
-    }}
->
-    <About />
-</Modal>
+    <Modal
+        width={30}
+        bind:this={aboutModal}
+        onClose={() => {
+            tabbedEditrorContents.focus()
+        }}
+    >
+        <About />
+    </Modal>
 
-<Toolbar>
-    <TabbedEditorButtons
-        ctx={tabbedEditorContext}
-        bind:this={tabbedEditorButtons}
-        on:newTab={openNewTabModal}
-    />
-    <Expand />
-    <PlaygroundLogo onClick={() => aboutModal.openModal()} />
-    <RunButton onClick={compileAndStartStatusPoll} disabled={isLoading} />
-</Toolbar>
+    <Toolbar>
+        <TabbedEditorButtons
+            ctx={tabbedEditorContext}
+            bind:this={tabbedEditorButtons}
+            on:newTab={openNewTabModal}
+        />
+        <Expand />
+        <PlaygroundLogo onClick={() => aboutModal.openModal()} />
+        <RunButton onClick={compileAndStartStatusPoll} disabled={isLoading} />
+    </Toolbar>
 
-<div class="box box-arrange-hor box-width-full box-height-full">
-    <TabbedEditorContents
-        ctx={tabbedEditorContext}
-        fontSize={17}
-        bind:this={tabbedEditrorContents}
-    />
+    <div class="box box-arrange-hor box-width-full box-height-full">
+        <TabbedEditorContents
+            ctx={tabbedEditorContext}
+            fontSize={17}
+            bind:this={tabbedEditrorContents}
+        />
 
-    <div class="box background-2d box-width-45">
-        <OutputPanel status={reactiveStatus} {output} bind:this={outputPanel} />
-        <InputPanel bind:this={inputPanel} />
+        <div class="box background-2d box-width-45">
+            <OutputPanel
+                status={reactiveStatus}
+                {output}
+                bind:this={outputPanel}
+            />
+            <InputPanel bind:this={inputPanel} />
+        </div>
     </div>
-</div>
+</main>
