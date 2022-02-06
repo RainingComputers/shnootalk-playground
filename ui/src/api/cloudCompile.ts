3e03272292e0d66f47fc3da3310208a8fe16ffe6
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-02-06 15:13:16 +0530

    Added file name validation

--- ui/src/api/cloudCompile.ts
+++ ui/src/api/cloudCompile.ts
@@ -28,10 +28,26 @@ const TIMED_OUT: CompileResponse = {
     result: CompileResult.EXEC_TIMEDOUT,
 }
 
+const INVALID_FILE_NAMES: CompileResponse = {
+    output: "File names should end with the extension '.shtk'",
+    result: CompileResult.COMPILE_FAILED,
+}
+
+export function validateFileNames(programs: { [key: string]: string }) {
+    for (const name in programs) {
+        if (name == "input") continue
+        if (!name.match(/\w+.shtk/)) return false
+    }
+
+    return true
+}
+
 export async function makeCompileRequest(programs: {
     [key: string]: string
 }): Promise<CompileResponse> {
     try {
+        if (!validateFileNames(programs)) return INVALID_FILE_NAMES
+        
         const response = await makeRequest(SERVER_URL, "post", programs)
 
         if (response.result === CompileResult.EXEC_TIMEDOUT) return TIMED_OUT
